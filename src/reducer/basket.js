73b48aba1ce5b6dc044322543e01d7da81fe4143
Tikhon Andrey <tikhon.andrey@gmail.com>
2017-02-01 18:25:16 +0300

    сохранение товаров в корзине после перезагрузки страницы

--- src/reducer/basket.js
+++ src/reducer/basket.js
@@ -2,18 +2,40 @@ import {
     ADD_TO_BASKET,
     DELETE_FROM_BASKET
 } from '../actions/shoping';
-import { arrayToMap, getPersistedState, persistState } from '../utils';
-import { Record, Map, List } from 'immutable';
+import {
+    arrayToMap,sequenceToMap,
+    getStateFromLocalStorage,
+    saveStateToLocalStorage } from '../utils';
+import { Record, Map, List, fromJS } from 'immutable';
 
 const BasketModel = Record({
     product: null,
     count:0
 });
-const defaultStateCreator = Record({
+
+const localStorageKey = 'basket';
+export class PersistRecord extends Record({
     entities: arrayToMap([], BasketModel),
-    total: 0   
-});
-const defaultState = getPersistedState('basket') || defaultStateCreator();
+    total: 0
+}) {
+    persistState(){
+        saveStateToLocalStorage(localStorageKey, this.toJS());
+        return this;
+    }
+    getPersistedState(key) {
+        const state = getStateFromLocalStorage(key);
+
+        if(state){
+            return this
+                .mergeIn(['entities'], sequenceToMap(state.entities, BasketModel))
+                .set('total', state.total)
+        }else{
+            return this;
+        }
+    }
+
+}
+const defaultState = new PersistRecord().getPersistedState(localStorageKey);
 
 export default (basket = defaultState, action) => {
     const { type, payload, response, error, generatedId } = action;
@@ -24,14 +46,16 @@ export default (basket = defaultState, action) => {
             if( basket.entities.has(id) ) {
                 return basket
                         .updateIn(['entities', id, 'count'], value=>value + 1)
-                        .update('total', value=>value + 1);
+                        .update('total', value=>value + 1)
+                        .persistState();
             }else{
                 return basket
                     .setIn(['entities', id], new BasketModel({
                         product: payload,
                         count: 1
                     }))
-                    .update('total', value=>value + 1);
+                    .update('total', value=>value + 1)
+                    .persistState();
             }
 
         case DELETE_FROM_BASKET:
@@ -40,11 +64,13 @@ export default (basket = defaultState, action) => {
                 if( count != 1 ){
                     return basket
                         .updateIn(['entities', payload, 'count'], value=>value-1)
-                        .update('total', value=>value - 1);
+                        .update('total', value=>value - 1)
+                        .persistState();
                 }else{
                     return basket
                         .deleteIn(['entities', payload])
-                        .update('total', value=>value - 1);
+                        .update('total', value=>value - 1)
+                        .persistState();
                 }
             }
 
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import { Map, Record, fromJS } from 'immutable'
+import _values from 'lodash/values';
 
 export function arrayToMap(arr, Model) {
     return arr.reduce((acc, el) => {
@@ -7,27 +8,37 @@ export function arrayToMap(arr, Model) {
     }, new Map({}))
 }
 
+export function sequenceToMap(seq, Model) {
+    return _values(seq).reduce((acc, el) => {
+        const immutableElement = Model ? new Model(el) : fromJS(el)
+        return acc.set(el.product.id, immutableElement)
+    }, new Map({}))
+}
+
 export const ReducerState = Record({
     entities: new Map({}),
     loading: false,
     loaded: false
-})
+});
 
 
-
-//todo persist
-export function persistState(key, state) {
-    window.localStorage.setItem(key, state)
+export function saveStateToLocalStorage(key, state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem(key, serializedState);
+    } catch (err) {
+        // Ignore write errors.
+    }
 }
 
-export function getPersistedState(key) {
-    const data = window.localStorage.getItem(key);
-
-    if(data){
-        return fromJS(
-            JSON.parse(data)
-        )
-    }else{
-        return null;
+export function getStateFromLocalStorage(key) {
+    try {
+        const serializedState = localStorage.getItem(key);
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
     }
 }
